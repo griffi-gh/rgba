15bcde74dd549d22e33100a737b140c444235fae
griffi-gh <prasol258@gmail.com>
2023-05-28 23:25:47 +0200

    simplify

--- chonk-arm/src/lib.rs
+++ chonk-arm/src/lib.rs
@@ -13,11 +13,4 @@ impl Cpu {
       reg: Registers::new()
     }
   }
-  pub fn _test(&mut self) {
-    use reg::*;
-    use gpr::*;
-    use mode::*;
-    self.reg.get_mode(Gpr::R15.with_mode(Mode::User));
-    self.reg.get_user(Gpr::R15);
-  }
 }
--- chonk-arm/src/reg.rs
+++ chonk-arm/src/reg.rs
@@ -1,27 +1,8 @@
-pub mod gpr;
-
-use gpr::{Gpr, ModeGpr};
-
 pub struct Registers {
-  gpr: [u32; ModeGpr::LEN]
 }
 
 impl Registers {
   pub fn new() -> Self {
-    Self {
-      gpr: [0; ModeGpr::LEN]
-    }
-  }
-  pub fn get_mode(&self, gpr: ModeGpr) -> u32 {
-    self.gpr[gpr as usize]
-  }
-  pub fn set_mode(&mut self, gpr: ModeGpr, value: u32) {
-    self.gpr[gpr as usize] = value;
-  }
-  pub fn get_user(&self, gpr: Gpr) -> u32 {
-    self.gpr[gpr as usize]
-  }
-  pub fn set_user(&mut self, gpr: Gpr, value: u32) {
-    self.gpr[gpr as usize] = value;
+    Self {}
   }
 }
