589dc39a140625f144845ca4a1c97711c7467c18
griffi-gh <prasol258@gmail.com>
2023-05-29 12:50:04 +0200

    remove explicit values

--- chonk-arm/src/mode.rs
+++ chonk-arm/src/mode.rs
@@ -1,11 +1,11 @@
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
 #[repr(u8)]
 pub enum Mode {
-  System = 0x81,
-  User = 0x80,
-  Fiq = 0,
-  Supervisor = 1,
-  Abort = 2,
-  Irq = 3,
-  Undefined = 4,
+  System,
+  User,
+  Fiq,
+  Supervisor,
+  Abort,
+  Irq,
+  Undefined,
 }
