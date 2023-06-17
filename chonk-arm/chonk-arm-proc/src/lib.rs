55c9ad9fbb87214e9d304d06b5d7c195be0f1b10
griffi-gh <prasol258@gmail.com>
2023-06-18 01:55:07 +0200

    add todo notice to `chonk-arm-proc`

--- chonk-arm/chonk-arm-proc/src/lib.rs
+++ chonk-arm/chonk-arm-proc/src/lib.rs
@@ -1,6 +1,8 @@
 extern crate proc_macro;
 use proc_macro::TokenStream;
 
+//TODO
+
 #[proc_macro]
 pub fn generate_arm_lut(_: TokenStream) -> TokenStream {
   "fn answer() -> u32 { 42 }".parse().unwrap()
