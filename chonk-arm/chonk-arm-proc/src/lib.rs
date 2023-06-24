4747c8a1548062302f7ae8d81a813b51ff9d05af
griffi-gh <prasol258@gmail.com>
2023-06-24 14:45:34 +0200

    wip proc

--- chonk-arm/chonk-arm-proc/src/lib.rs
+++ chonk-arm/chonk-arm-proc/src/lib.rs
@@ -1,7 +1,23 @@
-extern crate proc_macro;
 use proc_macro::TokenStream;
+use proc_macro2::TokenStream as TokenStream2;
+use quote::quote;
 
-//TODO
+mod decode_impl;
+
+#[derive(Clone, Copy, Debug, Default)]
+pub(crate) enum Exec {
+  #[default]
+  Panic
+}
+impl Exec {
+  pub fn tok(&self) -> TokenStream2 {
+    #[allow(unreachable_patterns)]
+    match self {
+      Self::Panic => quote!(exec::exec_panic),
+      _ => unimplemented!("Instruction handler for {self:?} is not implemented")
+    }
+  }
+}
 
 #[proc_macro]
 pub fn generate_arm_lut(_: TokenStream) -> TokenStream {
--- chonk-arm/chonk-arm/src/instructions.rs
+++ chonk-arm/chonk-arm/src/instructions.rs
@@ -1,8 +1,6 @@
 use crate::cpu::Cpu;
 
+mod exec;
+
 pub type ArmInstrHandler = fn(&mut Cpu, u32);
 pub type ThumbInstrHandler = fn(&mut Cpu, u32);
-
-pub fn exec_panic(cpu: &mut Cpu) {
-  panic!("panic handler; pc: {:08x}", cpu.reg.pc())
-}
