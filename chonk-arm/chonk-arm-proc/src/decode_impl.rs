cd68f12a62801316d84dfe910177db9b3b2c8af4
griffi-gh <prasol258@gmail.com>
2023-07-30 01:21:15 +0200

    my own fucking crate is borked???

--- chonk-arm/chonk-arm-proc/src/decode_impl.rs
+++ chonk-arm/chonk-arm-proc/src/decode_impl.rs
@@ -1,4 +1,4 @@
-use const_twiddle::Twiddle;
+use bit_field::BitField;
 use crate::{ExecArm, ExecThumb};
 
 pub(crate) fn decode_arm(instr: u32) -> ExecArm {
@@ -8,9 +8,9 @@ pub(crate) fn decode_arm(instr: u32) -> ExecArm {
 pub(crate) fn decode_thumb(instr: u32) -> ExecThumb {
   if instr & 0xf800 == 0x1800 {
     ExecThumb::AddSub {
-      sub: instr.bit(9),
-      imm: instr.bit(10),
-      rn: instr.bits(6..=7) as _,
+      sub: instr.get_bit(9),
+      imm: instr.get_bit(10),
+      rn: instr.get_bits(6..=7) as _,
     }
   } else {
     ExecThumb::Panic
